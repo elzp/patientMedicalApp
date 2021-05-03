22e537a94d79f587bffe2b9524af19a406a09f7a
Ela Piech <e-l-a-p@wp.pl>
2021-05-03 15:31:03 +0200

    functions to take care of saving new visit in json

--- src/dist/functions.js
+++ src/dist/functions.js
@@ -1,5 +1,4 @@
 "use strict";
-//const fs = require("fs")//.promises;
 var __spreadArrays = (this && this.__spreadArrays) || function () {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -9,7 +8,7 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
 };
 exports.__esModule = true;
 exports.updateAppoinments = void 0;
-var fs2 = require("fs").promises;
+var fs = require("fs");
 function modify(pacientid, obj, data) {
     var type = obj.type, name = obj.name, time = obj.time;
     var nData = __spreadArrays(data);
@@ -26,47 +25,48 @@ function modify(pacientid, obj, data) {
     }
     return nData;
 }
-// function write (data2: string){
-//     fs.writeFileSync("../appointByUser.json", data2, function(err: any) {
-//         if(err) {
-//             return console.log("jakiś error");
-//         }
-//         console.log("changes are saved");
-//     });
+function write(data2) {
+    fs.writeFile("../appointByUser.json", data2, function (err) {
+        if (err) {
+            return console.log("jakiś error");
+        }
+        console.log("changes are saved");
+    });
+}
+// export function updateAppoinments2(pacient_id:number, type: string, name: string, time: Date) {
+//     //sugestion of ΔO 'delta zero' in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+//     let obj = { type, name, time};
+//    //read file
+//    readFile("../appointByUser.json",'utf8')
+//     //change to js object
+//         .then((data0:string)=>JSON.parse(data0))
+//     //modyfy data
+//         .then((data: Array<in1>)=>modify(pacient_id,obj, data ))
+//     //change data to string
+//         .then((data2: Array<in1>)=>JSON.stringify(data2, null, 2))//Seth in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+//     //write data in file
+//         .then((body:string) => writeFile("../appointByUser.json", body))
+//         .catch((error:any) => console.warn(error))
 // }
 function updateAppoinments(pacient_id, type, name, time) {
-    //sugestion of ΔO 'delta zero' in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
     var obj = { type: type, name: name, time: time };
     //read file
-    fs2.readFile("../appointByUser.json", 'utf8')
-        //change to js object
-        .then(function (data0) { return JSON.parse(data0); })
-        //modyfy data
-        .then(function (data) { return modify(pacient_id, obj, data); })
-        //change data to string
-        .then(function (data2) { return JSON.stringify(data2, null, 2); }) //https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-        //write data in file
-        .then(function (body) { return fs2.writeFile("../appointByUser.json", body); })["catch"](function (error) { return console.warn(error); });
+    var data0 = fs.readFileSync("../appointByUser.json", 'utf8');
+    console.log(typeof data0);
+    var data = JSON.parse(data0);
+    console.log("data", data);
+    // console.log('data', data0,  data);
+    //modyfy data
+    var data2 = modify(pacient_id, obj, data); //.toString();
+    //  console.log('data2', data2);
+    //let data4 = JSON.stringify(data2);
+    //write data
+    var d = JSON.stringify(data2, null, 2); //.toString();//https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+    // console.log("is ok" //,data2, typeof data2.toString()
+    // , d)
+    write(d);
 }
 exports.updateAppoinments = updateAppoinments;
-//  export function updateAppoinments2(pacient_id:number, type: string, name: string, time: Date) {
-//      let obj = { type, name, time};
-//     //read file
-//     let data0 = fs.readFileSync("../appointByUser.json",'utf8');
-//     console.log(typeof data0)
-//     let data = JSON.parse(data0);
-//     console.log( "data",data)
-//    // console.log('data', data0,  data);
-//     //modyfy data
-//     let data2 = modify(pacient_id,obj, data )//.toString();
-//   //  console.log('data2', data2);
-//     //let data4 = JSON.stringify(data2);
-//     //write data
-//     let d =JSON.stringify(data2, null, 2)//.toString();//https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-//     // console.log("is ok" //,data2, typeof data2.toString()
-//     // , d)
-//    //// write(d);
-// }
 // updateAppoinments2(1,'type', 'name', new Date);
 //updateAppoinments(2,'type', 'name', new Date);
 module.exports = { updateAppoinments: updateAppoinments };
--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,8 @@
+import { writeFile, readFileSync } from 'fs';
+import * as fs from 'fs';
+//import {writeFile} fs = require("fs")//.promises;
+// const { readFile, writeFile } = require('fs').promises;
 
-//const fs = require("fs")//.promises;
-
-const fs2 = require("fs").promises;
 interface in0{
     vizId: number,
     type:string,
@@ -29,53 +30,52 @@ function modify (pacientid: number,obj:{type: string; name: string; time: Date;}
  return nData;
 }
 
-// function write (data2: string){
-//     fs.writeFileSync("../appointByUser.json", data2, function(err: any) {
-//         if(err) {
-//             return console.log("jakiś error");
-//         }
-//         console.log("changes are saved");
-//     });
+function write (data2: string){
+    fs.writeFile("../appointByUser.json", data2, function(err: any) {
+        if(err) {
+            return console.log("jakiś error");
+        }
+        console.log("changes are saved");
+    });
     
-    
-// }
-
-export function updateAppoinments(pacient_id:number, type: string, name: string, time: Date) {
-    //sugestion of ΔO 'delta zero' in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-    let obj = { type, name, time};
-   //read file
-   fs2.readFile("../appointByUser.json",'utf8')
-    //change to js object
-        .then((data0:string)=>JSON.parse(data0))
-    //modyfy data
-        .then((data: Array<in1>)=>modify(pacient_id,obj, data ))
-    //change data to string
-        .then((data2: Array<in1>)=>JSON.stringify(data2, null, 2))//Seth in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-    //write data in file
-        .then((body:string) => fs2.writeFile("../appointByUser.json", body))
-        .catch((error:any) => console.warn(error))
-}
-
-//  export function updateAppoinments2(pacient_id:number, type: string, name: string, time: Date) {
-//      let obj = { type, name, time};
-//     //read file
-//     let data0 = fs.readFileSync("../appointByUser.json",'utf8');
-//     console.log(typeof data0)
-//     let data = JSON.parse(data0);
-//     console.log( "data",data)
-//    // console.log('data', data0,  data);
+ }
+/*!!!error TypeError: fs.readFileSync is not a function - cannot use fs on browser-client side but only on server side; I must build mini express server 
+https://stackoverflow.com/questions/45466848/fs-readfilesync-is-not-a-function-meteor-react*/
+// export function updateAppoinments2(pacient_id:number, type: string, name: string, time: Date) {
+//     //sugestion of ΔO 'delta zero' in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+//     let obj = { type, name, time};
+//    //read file
+//    readFile("../appointByUser.json",'utf8')
+//     //change to js object
+//         .then((data0:string)=>JSON.parse(data0))
 //     //modyfy data
-//     let data2 = modify(pacient_id,obj, data )//.toString();
-//   //  console.log('data2', data2);
-//     //let data4 = JSON.stringify(data2);
-//     //write data
-//     let d =JSON.stringify(data2, null, 2)//.toString();//https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-//     // console.log("is ok" //,data2, typeof data2.toString()
-    
-//     // , d)
-//    //// write(d);
-    
+//         .then((data: Array<in1>)=>modify(pacient_id,obj, data ))
+//     //change data to string
+//         .then((data2: Array<in1>)=>JSON.stringify(data2, null, 2))//Seth in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+//     //write data in file
+//         .then((body:string) => writeFile("../appointByUser.json", body))
+//         .catch((error:any) => console.warn(error))
 // }
+
+ export function updateAppoinments(pacient_id:number, type: string, name: string, time: Date) {
+     let obj = { type, name, time};
+    //read file
+    let data0 =fs.readFileSync("../appointByUser.json",'utf8');
+    console.log(typeof data0)
+    let data = JSON.parse(data0);
+    console.log( "data",data)
+   // console.log('data', data0,  data);
+    //modyfy data
+    let data2 = modify(pacient_id,obj, data )//.toString();
+  //  console.log('data2', data2);
+    //let data4 = JSON.stringify(data2);
+    //write data
+    let d =JSON.stringify(data2, null, 2)//.toString();//https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
+    // console.log("is ok" //,data2, typeof data2.toString()
+    
+    // , d)
+    write(d);
+}
 
 // updateAppoinments2(1,'type', 'name', new Date);
 //updateAppoinments(2,'type', 'name', new Date);
