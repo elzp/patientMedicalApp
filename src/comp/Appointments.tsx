b6d4e5629dde42ce03f95085cfa07d3654a0db03
Ela Piech <e-l-a-p@wp.pl>
2021-05-03 15:51:49 +0200

    add axios function to send submited data to server

--- src/comp/Appointments.tsx
+++ src/comp/Appointments.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Appointment from './Appointment';
 import Select from 'react-select';//npm install --save @types/react-select
 import DatePicker from "react-datepicker";// npm i react-datepicker  ;  npm i @types/react-datepicker
-import { updateAppoinments } from '../dist/functions';
+import axios from 'axios';
 import './../App.css';
 import "react-datepicker/dist/react-datepicker.css";
 import data from './../somedata.json';
@@ -16,13 +16,13 @@ const [startDate, setStartDate] = useState(new Date());
 const [selectNameofDocVis, setVisNameOfDoc] = useState(false);
 const pacientid = 1;
 const newDoctors =
-  data.doctors.map(item => { 
+  data.doctors.map((item:any )=> { 
     let {option: value, value:label} = item;
     return {value, label};
 });
 
 const NamesOfDoctors = //prepare data in format {value label}.
-  data.doctors.map(item => { 
+  data.doctors.map((item:any )=> { 
     let {option: value, value:label} = item;
     return {value, label};
 });
@@ -44,9 +44,9 @@ function  handleTypeDoctorSelect(e:any| null | void){ //asynchronous function
   let newnamesOfDoctorsInGroup = 
   data.doctors
   //getting object with value = value2
-    .filter(item => item.value === e?.label || selectedOption[0])
+    .filter((item:any) => item.value === e?.label || item.value === selectedOption[0])
     //retreving array allDoctors
-    .map(item=>item.allDoctors);
+    .map((item:any)=>item.allDoctors);
  let newnamesOfDoctorsInGroup1= newnamesOfDoctorsInGroup[0];  
  let newnamesOfDoctorsInGroup2=
     Object.values(newnamesOfDoctorsInGroup1).map((it, ind) =>{ 
@@ -54,7 +54,7 @@ function  handleTypeDoctorSelect(e:any| null | void){ //asynchronous function
       let label =`${it}`;
       return { value, label };}) //create array with values type: {value, label}.
       ||namesOfDoctorsInGroup;//fix bug: not handling undefined or null
-  setNamesOfDoctorsInGroup(namesOfDoctorsInGroup=> newnamesOfDoctorsInGroup2);
+  setNamesOfDoctorsInGroup(namesOfDoctorsInGroup => newnamesOfDoctorsInGroup2);
 }
 
 function handleVisOfDocSel (){
@@ -65,6 +65,31 @@ function handleVisOfDocSel (){
     })
 }
 
+const visitApiAdress ="http://localhost:3000/newVisit";
+
+function onSubmit(e: any| null | void) {
+  e.preventDefault();
+  // get form data out of state
+  //const { first_name, last_name, password, email, phone } = this.state;
+//   fetch(`${visitApiAdress}`, {
+//   method: "POST",
+//   headers: {
+//     'Content-type': 'application/json'
+//   },
+//   body: JSON.stringify([{id:pacientid, type: selectedOption[0],name: selectedOption2[0], time:startDate}])
+// })
+// .then((response) => response.json())
+// .then((result) => {
+//   console.log(result)
+// })
+      axios
+      .post(`${visitApiAdress}`, {id:pacientid, type: selectedOption[0],name: selectedOption2[0], time:startDate})
+      .then(() => console.log('data was send'))
+      .catch((err: any) => {
+        console.error(err);
+      });
+
+}
   return (
     <div className="appointment">
 
@@ -81,30 +106,22 @@ function handleVisOfDocSel (){
     </div>
     
        
-    <form id="survey-form" //action="/submit-data"
+    <form id="survey-form" onSubmit={onSubmit}
     >   
     wybrana opcja w 1: {JSON.stringify(selectedOption)} ------  <br /> 
          grupa lekarzy: {JSON.stringify(namesOfDoctorsInGroup)} -----<br /> 
           wybrana opcja w 2: {JSON.stringify(selectedOption2)}
      <div className="div">Choose type of doctor:<br/></div>
-       <div>
-       {/* <Select
-        options={data.doctors}
-        //onChange={handleTypeSelect}
-      //   value={data.doctors.filter(function(option) {
-      //     return option.value === selectedOption;})
-      // }
-        //label="Single select"
-        /> */}
-         
+       <div>       
          <Select /*https://stackoverflow.com/questions/43250854/react-select-does-not-show-the-selected-value-in-the-field*/
         options={newDoctors}
         onChange={(e)=>{//https://stackoverflow.com/questions/26069238/call-multiple-functions-onclick-reactjs (multiple action functions)
+          handleTypeDoctorSelect(e);//set array of doctors
           handleDoctorSelect(e);//set value of first select
-          handleTypeDoctorSelect(e);} //set array of doctors
+          } 
         }
         onMenuClose ={handleVisOfDocSel}
-        value={newDoctors.find(function(option) {
+        value={newDoctors.find(function(option:any) {
           return option.value === selectedOption[0];
         })}
         label="Single select"
@@ -134,9 +151,7 @@ function handleVisOfDocSel (){
       dateFormat="MMMM d, yyyy h:mm aa"
     />
           
-          <div id="button"> <button  id="submit" type="submit"
-         onClick = {()=>updateAppoinments(pacientid, selectedOption[0], selectedOption2[0], startDate)}
-         >Submit</button></div> 
+          <div id="button"> <button  id="submit" type="submit">Submit</button></div> 
       </form>
      
       
