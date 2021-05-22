64f4c31016e8338d8fdaa5cd73c77da29bb0c9da
Ela Piech <e-l-a-p@wp.pl>
2021-05-22 22:09:02 +0200

    diplay data in every child component (appointment)

--- src/comp/Appointment.tsx
+++ src/comp/Appointment.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import Refferal from './Refferal';
 import './../App.css';
 
-function Appointment(props: any ) {
+function Appointment(props: any) {
  const {value}: {value: number} = props;
  //const {appointment} : {appointment: object} = props.styles;
 
+ const data =  props.dataAboutAppointment || {
+  "vizId": 1,
+  "type": "Gynecologist",
+  "name": "dr green Gynecologist",
+  "time": "2021-05-30T09:30:00.000Z"
+}
+ const {vizId, type, name, time} = data;
+
+
+
   return (
     <div className="appointment">
-
-         <p> This is new appointment {JSON.stringify( value)}.</p> 
+ 
+        {/* JSON.stringify( data) */}
+        <p>{vizId+1} appointment with {type}, dr <b>{name}</b>.</p> 
+        <p>Date:  {time.substr(0,10)}.</p>
+        <p>Time:  {time.substr(11,5)}.</p>       
         
          <Refferal noOfAppointment = {value}/>
        
