c273a9fc781c4947ae51596f1fd999ff8b14dfe8
Ela Piech <e-l-a-p@wp.pl>
2021-12-16 19:02:58 +0100

    Modify converting hours of appoinments

--- src/comp/Appointment.tsx
+++ src/comp/Appointment.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Refferal from './Refferal';
+import { DSTshiftedHour } from './srcfunctions';
 import './../App.css';
 
 function Appointment(props: any) {
@@ -13,18 +14,18 @@ function Appointment(props: any) {
   "time": "2021-05-30T09:30:00.000Z"
 }
  const {vizId, type, name, time} = data;
-
+  const date = new Date(time)
 
 
   return (
     <div className="appointment">
  
-        {/* JSON.stringify( data) */}
-        <p>{vizId+1} appointment with {type}, dr <b>{name}</b>.</p> 
-        <p>Date:  {time.substr(0,10)}.</p>
-        <p>Time:  {time.substr(11,5)}.</p>       
-        
-         {/* <Refferal noOfAppointment = {value}/> */}
+        {/* {JSON.stringify( DSTshiftedHour(date))} */}
+        <p>{vizId+1}. appointment with <span>{type}</span>:</p>
+        <p> dr <span>{name}</span>.</p> 
+        <p>Date:  {time.substr(0,10)}. Time:  {DSTshiftedHour(date) + time.substr(13,3)}.</p>
+        {/* <p>Time:  {time.substr(11,5)}.</p>        */}
+        {/* <Refferal noOfAppointment = {value}/> */}
        
        
    
