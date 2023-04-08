2b49ee8a3f8f7eb55299d17a5060c7ec631ea5a3
Ela Piech <e-l-a-p@wp.pl>
2023-04-08 19:16:28 +0200

    Clean code in functions.ts

--- server/dist/functions.js
+++ server/dist/functions.js
@@ -28,23 +28,6 @@ export function write(JSONData, path) {
         console.log("changes are saved");
     });
 }
-/*!!!error TypeError: fs.readFileSync is not a function - cannot use fs on browser-client side but only on server side; I must build mini express server
-https://stackoverflow.com/questions/45466848/fs-readfilesync-is-not-a-function-meteor-react*/
-// export function updateAppoinments2(pacient_id:number, type: string, name: string, time: Date) {
-//     //sugestion of ΔO 'delta zero' in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-//     let obj = { type, name, time};
-//    //read file
-//    readFile("../appointByUser.json",'utf8')
-//     //change to js object
-//         .then((data0:string)=>JSON.parse(data0))
-//     //modyfy data
-//         .then((data: Array<in1>)=>modify(pacient_id,obj, data ))
-//     //change data to string
-//         .then((data2: Array<in1>)=>JSON.stringify(data2, null, 2))//Seth in https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js
-//     //write data in file
-//         .then((body:string) => writeFile("../appointByUser.json", body))
-//         .catch((error:any) => console.warn(error))
-// }
 export function updateAppoinments(path, pacient_id, type, name, time) {
     let obj = { pacient_id, type, name, time };
     console.log('path:', path);
@@ -59,19 +42,3 @@ export function updateAppoinments(path, pacient_id, type, name, time) {
     console.log(modifiedData);
     console.log("finish updating appinintments");
 }
-// updateAppoinments2(1,'type', 'name', new Date);
-//updateAppoinments(2,'type', 'name', new Date);
-// [
-//     { "id": 0,
-//      "visits": [
-//          {"vizId": 0,
-//          "type" : "Gynecologist",
-//          "name": "dr yellow Gynecologist",
-//          "time": "time0"},
-//          {"vizId": 1,
-//              "type" : "Pediatrician",
-//              "name": "dr Grey Pediatrician",
-//              "time": "time1"}
-//      ]
-//     }
-//  ]
