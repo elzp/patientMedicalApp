0d32497730944653a3d1867cc463b73b012d13fb
Ela Piech <e-l-a-p@wp.pl>
2023-04-13 21:13:34 +0200

    Change in Acount: code for logging out

--- src/comp/Account.tsx
+++ src/comp/Account.tsx
@@ -1,51 +1,47 @@
 import React, { useState, useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import {logout} from './srcfunctions';
+import { Link, Navigate} from 'react-router-dom';
+import {handleLogout} from './srcfunctions';
 import './../App.css';
 import axios from 'axios';
+import { registerLocale } from 'react-datepicker';
 
 export function Account(props: any ) {
  const {value}: {value: number} = props;
  const { currentuser } = props.userdata;
- const { pacientId } = currentuser;
-
- //const {appointment} : {appointment: object} = props.styles;
+ const pacientId = localStorage.getItem('id');
+//  //const {appointment} : {appointment: object} = props.styles;
  // server API code
  const visitApiAdress ="http://localhost:3001";
 
- //set default value of variable pacientVisitsData
- const [pacientVisitsData, setpacientVisitsData] =useState({ "id": pacientId, "visits": []});
- 
- // set current data in pacientVisitsData from json file send from server
- function getdataFromFile(){
-   axios
-   .get(`${visitApiAdress}/${pacientId}`)
-   .then((res:any) => {
-   //log in browser
-   console.log('data was received', JSON.parse(res.data))
-   const data = JSON.parse(res.data);
-   setpacientVisitsData(pacientVisitsData => data)
-   }
- )
- .catch((err: any) => {
-   console.error(err);
- });
- }
- const [ifRefresh, setStatusIfRefresh] = useState(false)
- useMemo(()=> {getdataFromFile(); }, [ifRefresh])
- 
- function handleRefreshingVisits(){
-   setStatusIfRefresh(!ifRefresh);
- }
+
+ const handleClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+  e.preventDefault(); 
+const url = `${visitApiAdress}/user/logout/${pacientId}`.replace(/\"/g,'')
+await axios
+  .post(url, JSON.stringify({id: pacientId}))
+  .then((res:any) => {
+    console.log(JSON.parse(res.data))
+
+    handleLogout(props.defaultuser, 
+    props.changeuser, 
+    props.userdata);
+  })
+  .catch((err: any) => {
+    console.error(err);
+});
+}
+if (localStorage.getItem('isLogin')==="false") {
+  return <Navigate replace to="/" />;
+} else {
   return (
     <div className="account">
       <h3>Welcome {localStorage.getItem('nameOfLogin')}.</h3>
       <div>
         <Link reloadDocument to={'/'}>
-          <button onClick={()=>{logout()}}>LOGOUT</button>
+          <button onClick={(e)=>handleClick(e)}>LOGOUT</button>
         </Link>
       </div>
     </div>
   );
-}
+}}
 
